ac3dd86dfd260c6376fee366cc7252265ab15d8e
sebastian belettieri <sebastianbelettieri1@gmail.com>
2021-11-23 21:11:14 -0300

    Updates

---
+++ precache-manifest.49b7a76fe2f27a756d02989f32b634cd.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af77035728cce8d853d8a6b6bf50e2f4",
+    "url": "/TecnoSecurity/index.html"
+  },
+  {
+    "revision": "f661d4e327fff27c0b5c",
+    "url": "/TecnoSecurity/static/css/2.f3c24f35.chunk.css"
+  },
+  {
+    "revision": "1c3978f62e334cdeeb18",
+    "url": "/TecnoSecurity/static/css/main.44395572.chunk.css"
+  },
+  {
+    "revision": "f661d4e327fff27c0b5c",
+    "url": "/TecnoSecurity/static/js/2.8bddf874.chunk.js"
+  },
+  {
+    "revision": "092cfc00fdd8090c30f022af29ebbe4c",
+    "url": "/TecnoSecurity/static/js/2.8bddf874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c3978f62e334cdeeb18",
+    "url": "/TecnoSecurity/static/js/main.42dfa204.chunk.js"
+  },
+  {
+    "revision": "7f457550efe26b77b335",
+    "url": "/TecnoSecurity/static/js/runtime-main.105418e8.js"
+  },
+  {
+    "revision": "7acd9258b19a3dc03a71f7c0c1033b12",
+    "url": "/TecnoSecurity/static/media/404.7acd9258.png"
+  },
+  {
+    "revision": "af0292482d62d978c4e0be736ca500e7",
+    "url": "/TecnoSecurity/static/media/admin.af029248.png"
+  },
+  {
+    "revision": "c5a217c676e3d9ae0a5a3581f6f04800",
+    "url": "/TecnoSecurity/static/media/cctvkit.c5a217c6.jpg"
+  },
+  {
+    "revision": "42ca8dc7015765ab5ab2bb0ead5c1e0d",
+    "url": "/TecnoSecurity/static/media/cctvkitpcbox.42ca8dc7.jpg"
+  },
+  {
+    "revision": "df659da3215c0b035a0cdea35c5bcd85",
+    "url": "/TecnoSecurity/static/media/contact.df659da3.png"
+  },
+  {
+    "revision": "419b248f431932f391bf2c37ac6ccf2b",
+    "url": "/TecnoSecurity/static/media/default.419b248f.png"
+  },
+  {
+    "revision": "47bc07fab1e28f20080f4878844871ca",
+    "url": "/TecnoSecurity/static/media/dvr.47bc07fa.jpg"
+  },
+  {
+    "revision": "e7c82fc26c017c4f2bff04a9c3dc3e1a",
+    "url": "/TecnoSecurity/static/media/fb.e7c82fc2.jpg"
+  },
+  {
+    "revision": "b5ef59ddb93f0adfabab4c16ac29e6dc",
+    "url": "/TecnoSecurity/static/media/ig.b5ef59dd.jpg"
+  },
+  {
+    "revision": "ad593eeee11164533cdb8478cc38387c",
+    "url": "/TecnoSecurity/static/media/location.ad593eee.png"
+  },
+  {
+    "revision": "c4eab4aef31cf79b812dcbb14b55ac3d",
+    "url": "/TecnoSecurity/static/media/preloadWhite.c4eab4ae.gif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/TecnoSecurity/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/TecnoSecurity/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/TecnoSecurity/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/TecnoSecurity/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4b9ca08f8a65e4462ce2d62d73ff65aa",
+    "url": "/TecnoSecurity/static/media/slider.4b9ca08f.jpg"
+  },
+  {
+    "revision": "a11a1cb050fbc47070c9a795756a99ce",
+    "url": "/TecnoSecurity/static/media/wp.a11a1cb0.jpg"
+  }
+]);
