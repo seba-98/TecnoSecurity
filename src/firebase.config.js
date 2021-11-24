5286c57d20808abab2f8e2f8cbb9aac29289230e
sebastian belettieri <sebastianbelettieri1@gmail.com>
2021-11-24 20:35:41 -0300

    changer firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -9,6 +9,20 @@ import "firebase/compat/firestore"
 
 // Your web app's Firebase configuration
 export const app= firebase.initializeApp({
+  apiKey: "tecnosecurity-fbd19",
+  authDomain: "AIzaSyBIa3b-Pn7-skSvnP1g1JEVX1urKM6bTpI",
+  projectId: "968252073629:web:afc9d6de638a3952e9d674",
+  storageBucket: "tecnosecurity-fbd19.firebaseapp.com",
+  messagingSenderId: "tecnosecurity-fbd19.appspot.com",
+  appId: "968252073629",
+  locationId: "us-central",
+});
+
+// Initialize Firebase
+export const db = getFirestore(app); 
+
+
+/* export const app= firebase.initializeApp({
   "apiKey": process.env.REACT_APP_FIREBASE_API_KEY,
   "authDomain": process.env.REACT_APP_FIREBASE_AUTO_DOMAIN,
   "projectId": process.env.REACT_APP_FIREBASE_PROYECT_ID,
@@ -16,8 +30,4 @@ export const app= firebase.initializeApp({
   "messagingSenderId": process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   "appId": process.env.REACT_APP_FIREBASE_APP_ID,
   "locationId": "us-central",
-});
-
-// Initialize Firebase
-export const db = getFirestore(app); 
-
+}); */
